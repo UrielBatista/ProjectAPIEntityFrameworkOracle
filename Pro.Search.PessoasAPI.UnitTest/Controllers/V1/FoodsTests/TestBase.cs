4bfcdd4d9dcb66ffc62d8b0db0e57548a78c719c
uriel batista <uri.batista@hotmail.com>
2021-12-08 00:04:27 -0300

    Create UnitTest to FoodController.

---
+++ Pro.Search.PessoasAPI.UnitTest/Controllers/V1/FoodsTests/TestBase.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
+using PessoasAPI.Controllers.V1;
+
+namespace Pro.Search.PessoasAPI.UnitTest.Controllers.V1.FoodsTests
+{
+    [TestClass]
+    public class TestBase
+    {
+        protected static FoodController CreateController(IMediator mediator)
+            => new FoodController(mediator ?? Substitute.For<IMediator>());
+    }
+}
