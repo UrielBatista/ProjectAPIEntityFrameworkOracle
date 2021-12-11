c85bc8e0bd7c02809698472ebb7d205873870dcd
Uriel Batista <ur.sabatista@gmail.com>
2021-12-11 12:18:30 -0300

    Add CommandTest to GetPersonPurcashFood

---
+++ Pro.Search.PessoasAPI.UnitTest/Controllers/V1/PessoasPurcashControllerTests/TestBase.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
+using PessoasAPI.Controllers.V1;
+
+namespace Pro.Search.PessoasAPI.UnitTest.Controllers.V1.PessoasPurcashControllerTests
+{
+    [TestClass]
+    public class TestBase
+    {
+        protected static PessoasPurcashController CreateController(IMediator mediator)
+        => new PessoasPurcashController(mediator ?? Substitute.For<IMediator>());
+    }
+}
