bc9cad638df90b4efa6f61dc44bfb83874facf19
UrielBatista <ur.sabatista@gmail.com>
2023-03-15 17:19:16 -0700

    Add rules to Existis person in DataLoad

---
+++ Pro.Search.Infraestructure/GraphQL/DataLoaders/ValidityFlagPersonExistDataLoad.cs
@@ -0,0 +1,70 @@
+﻿using GreenDonut;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Threading;
+using Pro.Search.Infraestructure.Repositories;
+using Pro.Search.PersonDomains.PersonEngine.Dtos;
+using AutoMapper;
+using System.Linq;
+using ServiceStack;
+
+namespace Pro.Search.Infraestructure.GraphQL.DataLoaders
+{
+    public class ValidityFlagPersonExistDataLoad : BatchDataLoader<RequestIdsDto, ValidityFlagDto>
+    {
+        private readonly IPersonsRepository _repository;
+        private readonly IMapper _mapper;
+
+        public ValidityFlagPersonExistDataLoad(
+            IPersonsRepository repository,
+            IMapper mapper,
+            IBatchScheduler batchScheduler,
+            DataLoaderOptions options = null)
+            : base(batchScheduler, options)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        protected override async Task<IReadOnlyDictionary<RequestIdsDto, ValidityFlagDto>> LoadBatchAsync(IReadOnlyList<RequestIdsDto> keys, CancellationToken cancellationToken)
+        {
+            var idsPersonAndFood = keys
+            .Select(k => new { k.IdPerson, k.IdFood })
+            .Distinct()
+            .ToList();
+
+            var result = new ValidityFlagDto(default, default);
+
+            var data = await _repository.FindPersonPurcashFood(
+                idsPersonAndFood.Select(x => x.IdPerson).FirstOrDefault(), 
+                cancellationToken).ConfigureAwait(false);
+
+            if (data != null)
+            {
+                result.ExistPerson = true;
+
+                var checkIfExistFoodPurcachedInPerson = data.ComidaComprada.ToArray();
+
+                if (checkIfExistFoodPurcachedInPerson.Any())
+                {
+                    result.ExistFood = true;
+                }
+                else
+                {
+                    result.ExistFood = false;
+                }
+            }
+            else
+            {
+                result.ExistPerson = false;
+                result.ExistFood = false;
+            }
+
+            var fruitQuantities = new Dictionary<RequestIdsDto, ValidityFlagDto>
+            {
+                { keys.FirstOrDefault(), result }
+            };
+            return fruitQuantities;
+        }
+    }
+}
--- Pro.Search.Infraestructure/GraphQL/Queries/PersonQueryHotChocolate.cs
+++ Pro.Search.Infraestructure/GraphQL/Queries/PersonQueryHotChocolate.cs
@@ -1,4 +1,5 @@
-﻿using HotChocolate;
+﻿using GreenDonut;
+using HotChocolate;
 using HotChocolate.Data;
 using Pro.Search.Infraestructure.Context;
 using Pro.Search.Infraestructure.GraphQL.DataLoaders;
@@ -6,6 +7,7 @@ using Pro.Search.PersonDomains.PersonEngine.Dtos;
 using Pro.Search.PersonDomains.PersonEngine.Entities;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Pro.Search.Infraestructure.GraphQL.Queries
@@ -24,8 +26,11 @@ namespace Pro.Search.Infraestructure.GraphQL.Queries
         public IQueryable<Food> GetFoodPurchase([Service] ISystemDBContext context) =>
             context.Food;
 
-        public async Task<List<PersonsAllInfoDto>> GetPerson(string id, PersonDataLoad dataLoader) => 
+        public async Task<List<PersonsAllInfoDto>> GetPerson(string id, PersonDataLoad dataLoader) =>
             await dataLoader.LoadAsync(id);
-    }
 
+        public async Task<ValidityFlagDto> ValidFlagsPersonExist(RequestIdsDto requestIds,
+            ValidityFlagPersonExistDataLoad dataLoader) =>
+            await dataLoader.LoadAsync(requestIds);
+    }
 }
