65bdf310c80f979e73435ebfdea9c68c41ebd15e
UrielBatista <49211381+UrielBatista@users.noreply.github.com>
2023-04-15 17:52:58 -0300

    Add custom GC in GraphQL Batching requests (#17)

--- Pro.Search.Infraestructure/GraphQL/Queries/PersonQueryHotChocolate.cs
+++ Pro.Search.Infraestructure/GraphQL/Queries/PersonQueryHotChocolate.cs
@@ -1,13 +1,12 @@
-﻿using GreenDonut;
-using HotChocolate;
+﻿using HotChocolate;
 using HotChocolate.Data;
 using Pro.Search.Infraestructure.Context;
 using Pro.Search.Infraestructure.GraphQL.DataLoaders;
 using Pro.Search.PersonDomains.PersonEngine.Dtos;
 using Pro.Search.PersonDomains.PersonEngine.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading;
 using System.Threading.Tasks;
 
 namespace Pro.Search.Infraestructure.GraphQL.Queries
@@ -17,14 +16,22 @@ namespace Pro.Search.Infraestructure.GraphQL.Queries
         [UseProjection]
         [UseFiltering]
         [UseSorting]
-        public IQueryable<Persons> GetPersonsData([Service] ISystemDBContext context) =>
-            context.Pessoas;
+        public IQueryable<Persons> GetPersonsData([Service] ISystemDBContext context)
+        {
+            var queryable = context.Pessoas;
+            DisposeIfDisposable(ref queryable);
+            return queryable;
+        }
 
         [UseProjection]
         [UseFiltering]
         [UseSorting]
-        public IQueryable<Food> GetFoodPurchase([Service] ISystemDBContext context) =>
-            context.Food;
+        public IQueryable<Food> GetFoodPurchase([Service] ISystemDBContext context)
+        {
+            var result = context.Food;
+            DisposeIfDisposable(ref result);
+            return result;
+        }
 
         public async Task<List<PersonsAllInfoDto>> GetPerson(string id, PersonDataLoad dataLoader) =>
             await dataLoader.LoadAsync(id);
@@ -32,5 +39,17 @@ namespace Pro.Search.Infraestructure.GraphQL.Queries
         public async Task<ValidityFlagDto> ValidFlagsPersonExist(RequestIdsDto requestIds,
             ValidityFlagPersonExistDataLoad dataLoader) =>
             await dataLoader.LoadAsync(requestIds);
+
+        public static void DisposeIfDisposable<T>(ref T obj)
+        {
+            if (obj is IDisposable disposable)
+            {
+                disposable.Dispose();
+                obj = default(T);
+            }
+
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+        }
     }
 }
